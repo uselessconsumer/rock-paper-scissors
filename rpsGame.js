c8db780457928af332517b0688ca47a198a313f2
uselessconsumer <eliottdale@gmail.com>
2023-08-08 17:08:45 -0400

    Outline algorithm in comments

---
+++ rpsGame.js
@@ -0,0 +1,18 @@
+// Ask the user for their choice: rock, paper, or scissors
+//Generate the computers choice: rock, paper, or scissors
+//Evaluate the choice hierarchy:
+    //Rock beats scissors
+    //Scissors beats paper
+    //Paper beats rock
+//Compare user and computers move choice
+    //Determine winner
+//State who has won
+//Display the chosen moves of both the user and computer
+
+
+
+
+
+function getComputerChoice () {
+
+}
