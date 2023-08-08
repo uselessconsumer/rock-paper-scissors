e637f18f782cb2099b3053e2a4bd2a5ab01fbe74
uselessconsumer <eliottdale@gmail.com>
2023-08-08 18:06:40 -0400

    create a function to generate the computers choice

--- rpsGame.js
+++ rpsGame.js
@@ -6,13 +6,36 @@
     //Paper beats rock
 //Compare user and computers move choice
     //Determine winner
-//State who has won
+//State who has won or if there is a tie
 //Display the chosen moves of both the user and computer
+//Prompt the user if they would like to play again:
+//If no: end the program
+//If yes: clear the console and run again
 
 
 
+//Generate the computers choice: rock, paper, or scissors
+    //Generate a number between 0 to 2
+        //Assign 0 to rock
+        //Assign 1 to paper
+        //Assign 2 to scissors
 
+let computerChoice;
+let playerChoice;
 
-function getComputerChoice () {
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+
+    if (randomChoice === 0) {
+        return 'rock';
+    } else if (randomChoice === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+function playGame() {
 
 }
+console.log(getComputerChoice());
