1ddf0e7a57131bf4c32f2ce59e1ab8a3ad49624a
uselessconsumer <eliottdale@gmail.com>
2023-08-23 16:02:01 -0400

    modify the DOM to display round number and computer/player wins

--- rpsGame.js
+++ rpsGame.js
@@ -23,81 +23,92 @@ function game() {
         }
     }
 
-    //Create variables to count the Round number, player wins and computer wins
-    var roundCount;
-    var playerWin;
-    var computerWin;
-
-
     // for (roundCount = 0, playerWin = 0, computerWin = 0
     //     ; roundCount < 5 && playerWin < 3 && computerWin < 3
     //     ; ) {
-    
-    //Console log the player selection
+    //         console.log('Round number: ' + roundCount);
+    //         console.log('Player wins: ' + playerWin);
+    //         console.log('Computer win: ' + computerWin);
+    //     };
 
-        // console.log(playRound());
-        // console.log('Round number: ' + roundCount);
-        // console.log('Player wins: ' + playerWin);
-        // console.log('Computer win: ' + computerWin);
+    var roundCount = 0;
+    var playerWin = 0;
+    var computerWin = 0;
+
+    const buttons = document.querySelectorAll('button');
+
+    const results = document.querySelector('.results')
+    const result = document.createElement('p');
+    const playerWinCount = document.createElement('p');
+    const computerWinCount = document.createElement('p');
+    const totalRoundCount = document.createElement('p');
 
 
-        const buttons = document.querySelectorAll('button');
-        const result = document.querySelector('.result')
+    results.appendChild(result);
+    results.appendChild(playerWinCount);
+    results.appendChild(computerWinCount);
+    results.insertBefore(totalRoundCount, playerWinCount);
+
+    playerWinCount.textContent = 'Player Wins: ' + playerWin;
+    computerWinCount.textContent = 'Computer Wins: ' + computerWin;
+    totalRoundCount.textContent = 'Round Number: ' + roundCount;
 
 
-        buttons.forEach((button) => {
-            button.addEventListener('click', () => {
-                result.textContent = playRound(button.id);
-            });
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            result.textContent = playRound(button.id)
+            playerWinCount.textContent = 'Player Wins: ' + playerWin;
+            computerWinCount.textContent = 'Computer Wins: ' + computerWin;
+            totalRoundCount.textContent = 'Round Number: ' + roundCount;
         });
+    });
+
+    
 
 
+    function playRound(playerChoice, computerChoice = getComputerChoice()) {
+            
+        //If there is a draw
+        if (playerChoice === computerChoice) {
+            return `Draw! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} ties with ${computerChoice}.`;
 
-        function playRound(playerChoice, computerChoice = getComputerChoice()) {
+        } else if (playerChoice === 'rock') {
+            if (computerChoice === 'paper') {
+                computerWin++;
+                roundCount++;
+                return 'You lose! Paper beats rock.'; 
                 
-            //If there is a draw
-            if (playerChoice === computerChoice) {
-                return `Draw! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} ties with ${computerChoice}.`;
-
-            } else if (playerChoice === 'rock') {
-                if (computerChoice === 'paper') {
-                    computerWin++;
-                    roundCount++;
-                    return 'You lose! Paper beats rock.'; 
-                    
-                } else if (computerChoice === 'scissors'); {
-                    playerWin++;
-                    roundCount++;
-                    return 'You win! Rock beats scissors';
-                }
-
-            } else if (playerChoice === 'paper') {
-                if (computerChoice === 'rock') {
-                    playerWin++;
-                    roundCount++;
-                    return 'You win! Paper beats rock.';
-                } else if (computerChoice === 'scissors'); {
-                    computerWin++;
-                    roundCount++;
-                    return 'You lose! Scissors beats paper.';
-                }
-
-            } else if (playerChoice === 'scissors') {
-                if (computerChoice === 'rock') {
-                    computerWin++;
-                    roundCount++;
-                    return 'You lose! Rock beats scissors.';
-                } else if (computerChoice === 'paper'); {
-                    playerWin++;
-                    roundCount++;
-                    return 'You win! Scissors beat paper.';
-                }
-            } else {
-                return 'Please enter a valid move.'
+            } else if (computerChoice === 'scissors'); {
+                playerWin++;
+                roundCount++;
+                return 'You win! Rock beats scissors';
             }
-        }
 
-    // }
+        } else if (playerChoice === 'paper') {
+            if (computerChoice === 'rock') {
+                playerWin++;
+                roundCount++;
+                return 'You win! Paper beats rock.';
+            } else if (computerChoice === 'scissors'); {
+                computerWin++;
+                roundCount++;
+                return 'You lose! Scissors beats paper.';
+            }
+
+        } else if (playerChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                computerWin++;
+                roundCount++;
+                return 'You lose! Rock beats scissors.';
+            } else if (computerChoice === 'paper'); {
+                playerWin++;
+                roundCount++;
+                return 'You win! Scissors beat paper.';
+            }
+        } else {
+            return 'Please enter a valid move.'
+        }
+    }
 
     //Display a message to the user stating wether they won or lost the BO5
     // let finalWinner = (playerWin === 3)
